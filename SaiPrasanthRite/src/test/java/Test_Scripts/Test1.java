66502ff00bad4c4d24964f9f9afb321aa73cd93f
sai prasanth <sai prasanth@DESKTOP-488O7GK>
2024-01-28 20:34:42 +0530

    initial commit

---
+++ SaiPrasanthRite/src/test/java/Test_Scripts/Test1.java
@@ -0,0 +1,12 @@
+package Test_Scripts;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String s= "SaiPrasanthRite";
+		System.out.println(s);
+	}
+
+}
